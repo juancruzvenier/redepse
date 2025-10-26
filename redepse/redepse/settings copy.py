e4d114236e9b71e41adde53ed20bc1c4ad83e15e
MarsProm <perezledesmamariano@gmail.com>
2025-10-26 19:05:37 -0300

    ahora si base de datos oculta , agrugue un requirement mas

--- redepse/redepse/settings copy.py
+++ redepse/redepse/settings copy.py
@@ -81,14 +81,15 @@ WSGI_APPLICATION = 'redepse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'jcbBqzJuWsCacFkhYbimbRwFxaUtwZHJ',
+        'HOST': 'shuttle.proxy.rlwy.net',
+        'PORT': '10538',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
